1e9d63e636dc149ecd0b8f3afcdfbc4fa41706a5
SKY <tkrhddbf@gmail.com>
2022-11-09 14:51:35 +0900

    ajax&json 통신

---
+++ MVC07/src/kr/bit/controller/MemberAjaxDeleteController.java
@@ -0,0 +1,27 @@
+package kr.bit.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.bit.model.MemberDAO;
+
+public class MemberAjaxDeleteController implements Controller {
+
+	@Override
+	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("");
+		int num=Integer.parseInt(request.getParameter("num"));
+		System.out.println(num);
+		
+		MemberDAO dao=new MemberDAO();
+		int cnt=dao.memberDelete(num);
+		response.getWriter().print(cnt);
+		
+		return null;
+	}
+
+}
---
+++ MVC07/src/kr/bit/frontcontroller/HandlerMapping.java
@@ -0,0 +1,29 @@
+package kr.bit.frontcontroller;
+
+import java.util.HashMap;
+
+import kr.bit.controller.*;
+
+public class HandlerMapping {
+  private HashMap<String, Controller> mappings;
+  public HandlerMapping() {
+	  mappings=new HashMap<String, Controller>();
+	  mappings.put("/memberList.do", new MemberListController());
+	  mappings.put("/memberInsert.do", new MemberInsertController());
+	  mappings.put("/memberRegister.do", new MemberRegisterController());
+	  mappings.put("/memberContent.do", new MemberContentController());
+	  mappings.put("/memberUpdate.do", new MemberUpdateController());
+	  mappings.put("/memberDelete.do", new MemberDeleteController());
+	  mappings.put("/memberLogin.do", new MemberLoginController());
+	  mappings.put("/memberLogout.do", new MemberLogoutController());	 
+	  mappings.put("/memberDbcheck.do", new MemberDbcheckController());
+	  //memberAjaxList.do
+	  mappings.put("/memberAjaxList.do", new MemberAjaxListController());
+	  //memberAjaxDelete.do
+	  mappings.put("/memberAjaxDelete.do", new MemberAjaxDeleteController());
+	  
+  }
+  public Controller getController(String key) { // key=>/memberList.do
+	  return mappings.get(key);
+  }
+}
